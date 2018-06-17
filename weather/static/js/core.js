a2c7ae10bb87bfc82961bc5fb786e949ace937f3
Tim Lin <tarry21304@gmail.com>
2018-06-17 14:43:20 +0800

    ValueText and little MouseEvent

--- weather/static/js/core.js
+++ weather/static/js/core.js
@@ -1,12 +1,11 @@
 // 代辦事項
-// station有自己的顏色
+// station有自己的顏色(down)
 // 滑鼠過去有神秘直線
 // 看一下人家怎麼人多條線顯示得比較好
-var station = 'Banqiao';
+var station = d3.select('.first_plot').select("#menu #station_select").property('value');
 var attribute = 'AMB_TEMP';
 var selectValue, selectTime;
-var color = ['rgba(255, 172, 0, 0.8)', 'rgba(12, 12, 34, 0.8)','rgba(255, 0, 0, 0.8)','rgba(0, 255, 0, 0.8)','rgba(0, 0, 255, 0.8)']
-    coclorCount = 0;
+var stationDictionary = {};
 
 $.getScript("/static/js/TimeChart.js", function(){
     $.ajax({
@@ -31,4 +30,24 @@ $.getScript("/static/js/TimeChart.js", function(){
     });
 });
 
+var stationCount = 0;
+
+d3.selectAll('.first_plot #menu #station_select option').style('color', function(d,i){return 'hsl(' + i*12 + ',100%, 40%)'})
+                                                        .attr('id', function(d,i){ stationCount++; return 'option'+i;});
+d3.selectAll('.second_plot #menu #station_select option').style('color', function(d,i){return 'hsl(' + i*12 + ',100%, 40%)'})
+                                                        .attr('id', function(d,i){ return i;});
+
+var j = 0
+for (j; j < stationCount; j++){
+    var pickStation = d3.select('.first_plot #menu #station_select #option'+j).property('value');
+    stationDictionary[pickStation] = j;
+}
+
+$.getScript("/static/js/ValueText.js", function(){
+    addText('Date', '.first_plot');
+    addText(station, '.first_plot');
+    addText('Date', '.second_plot');
+    addText(station, '.second_plot');
+});
+$.getScript("/static/js/MouseEvent.js");
 $.getScript("/static/js/PlotControl.js");
